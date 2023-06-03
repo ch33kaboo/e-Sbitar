e0fde7fd946e6369b9fb479a035be474c1b37618
ch33kaboo <mc.ouzane@esi-sba.dz>
2023-06-03 21:42:11 +0100

    added register functionality

--- src/SimpleHttpServer.java
+++ src/SimpleHttpServer.java
@@ -1,6 +1,10 @@
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -8,19 +12,48 @@ import com.sun.net.httpserver.HttpServer;
 public class SimpleHttpServer {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/", new MyHandler());
+        server.createContext("/user", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
 
     static class MyHandler implements HttpHandler {
+        private List<String> users = new ArrayList<>();
+
         @Override
         public void handle(HttpExchange t) throws IOException {
-            String response = "Hello";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            if ("POST".equals(t.getRequestMethod())) {
+                String requestBody = readRequestBody(t.getRequestBody());
+                users.add(requestBody);
+                String response = "User added";
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            } else {
+                // String response = "Method not allowed";
+                // t.sendResponseHeaders(405, response.length());
+                // OutputStream os = t.getResponseBody();
+                // os.write(response.getBytes());
+                // os.close();
+
+                // return array of all the users
+                String response = users.toString();
+                t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            }
+        }
+
+        private String readRequestBody(InputStream is) throws IOException {
+            ByteArrayOutputStream result = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) != -1) {
+                result.write(buffer, 0, length);
+            }
+            return result.toString("UTF-8");
         }
     }
 }
